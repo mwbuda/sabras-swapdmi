1d06a76b99b93b8bd75b046cfb16372c240f1c59
mwbuda <mbuda@sabrassoft.com>
2015-09-03 15:17:51 -0400

    2nd cut of session handling, still untested. design is both simpler & has more potential capabilities: * optional session search (NO BASE IMPLEMENTATION) * default built in simple session store * API to get sessions by sid or uid * simplified accessing sessionHandling, you get the object 	instead of a calling a convience method that 	delegates to the handling object * change 'session-track' naming to 'session-handling' as much as possible * reorganized SessionHandling method declarations, to be grouped.

--- lib/swapdmi/ext/sessiontrack.rb
+++ lib/swapdmi/ext/sessiontrack.rb
@@ -3,12 +3,22 @@
 #
 # adds specialized session tracking support
 #
+# This extension integrates with the base logging extension, if that extension is present.
+#	SessionHandling instances will have access to logging
 #
+# similar in execution to the basic logging extension, with the exception
+#	that all session handling activities must go to the linked SessionHandling object,
+#	there is no convient parseSession() methods on Model, ContextOfUse, etc.
 #
+# the default configuration stores sessions by session-id & user-id
+#	in a shared 2-level hash (shared thru-out SwapDmi module)
+# a user may only have 1 open session at a time, closed sessions are immediatly deleted from the session-store 
 #
+# the extension supports API calls for complex search over sessions, but does not implement anything by default
+#	if you want/need this capability, you will need to write it yourself
 #
 module SwapDmi
-	SwapDmi.declareExtension(:sessiontrack) 
+	SwapDmi.declareExtension(:sessionhandle) 
 	
 	class SessionInfo
 		attr_reader :id, :uid, :expire, :sundry
@@ -27,9 +37,31 @@ module SwapDmi
 		
 	end
 	
-	DefaultSessionTracking = Proc.new {|session| session}
-	DefaultSessionParsing = Proc.new do |raw| 
-		SwapDmi::SessionInfo.new(raw[:id], raw[:uid], raw[:expire])
+	DefaultSessionStore = {
+		:sid => {},
+		:uid => {}
+	}
+	
+	DefaultSessionFetchForUser = Proc.new do |uid, sundry|
+		SwapDmi::DefaultSessionStore[:uid][uid]
+	end
+	
+	DefaultSessionFetchById = Proc.new do |sid|
+		SwapDmi::DefaultSessionStore[:sid][sid]
+	end
+	
+	DefaultSessionParsing = Proc.new do |raw|
+		SwapDmi::SessionInfo.new(
+			raw[:sid], raw[:uid], raw[:expire]
+		).withSundry(raw)
+	end
+	
+	DefaultSessionTracking = Proc.new do |session|
+		old = SwapDmi::DefaultSessionStore[:uid][session.uid]
+		SwapDmi::DefaultSessionStore[:sid].delete(old.id)
+		SwapDmi::DefaultSessionStore[:uid][uid] = session
+		SwapDmi::DefaultSessionStore[:sid][session.id] = session
+		session
 	end
 	
 	class SessionHandling
@@ -41,28 +73,56 @@ module SwapDmi
 			assignId(id)
 		end
 		
+		def defineFetchForUser(&fetch)
+			@userFetch = fetch.nil? ? SwapDmi::DefaultSessionFetchForUser : fetch
+			@userFetch = fetch
+			self
+		end
+		def fetchForUser(uid, sundry = {})
+			@userFetch = SwapDmi::DefaultSessionFetchForUser if @userFetch.nil?
+			@userFetch.call(uid, sundry)
+		end
+		
+		def defineFetch(&fetch)
+			@idFetch = fetch.nil? ? SwapDmi::DefaultSessionFetchById : fetch
+			self
+		end
+		def fetch(sid)
+			@idFetch = SwapDmi::DefaultSessionFetchById if @idFetch.nil?
+			@idFetch.call(sid)
+		end
+		
+		def canSearch?()
+			!@search.nil?
+		end
+		def defineSearch(&search)
+			@search = search
+			self
+		end
+		def search(criteria = {})
+			@search.call(criteria)
+		end
+		
 		def defineParsing(&parse)
 			@parse = parse.nil? ? SwapDmi::DefaultSessionParsing : parse
 			self
+		end
+		def parse(raw)
+			@parse = SwapDmi::DefaultSessionParsing if @parse.nil?
+			@parse.call(raw)
 		end
 		
 		def defineTracking(&track)
 			@track = track.nil? ? SwapDmi::DefaultSessionTracking : track
 			self
 		end
-		
-		def parse(raw)
-			@parse = DefaultSessionParsing if @parse.nil?
-			@parse.call(raw)
-		end
-		
-		def trackSession(session)
+		def track(session)
 			@track = DefaultSessionTracking if @track.nil?
 			@track.call(session)
 			session
 		end
 		
-		def parseTrackSession(raw)
+		def parseTrack(raw)
 			self.trackSession(self.parseSession(raw))
 		end
 	end
@@ -80,46 +140,33 @@ module SwapDmi
 			base.instance_eval { include SwapDmi::HasSessionHandling::Instance }
 		end
 		
-		def sessionHandling()
+		def allSessionHandling()
 			self.class_variable_get(:@@sessionHandler)
 		end
 		
-		def defineSessionHandler(id, shid)
+		def defineSessionHandling(id, shid)
 			self.logging[id] = shid
 			self
 		end
 		
-		def sessionHandler(id)
-			shid = self.sessionHandling[id]
+		def sessionHandling(id)
+			shid = self.allSessionHandling[id]
 			SwapDmi::SessionHandling[shid]
 		end
 		
 		module Instance
 			
 			def defineSessionHandler(shid)
-				self.class.sessionHandling[self.id] = shid
+				self.class.allSessionHandling[self.id] = shid
 				self
 			end
 			
-			def sessionHandler()
-				shid = self.class.sessionHandling[self.id]
+			def sessionHandling()
+				shid = self.class.allSessionHandling[self.id]
 				SwapDmi::SessionHandling[shid]
 			end
-			
-			def parseSession(raw)
-				self.sessionHandler.parseSession(raw)
-			end
-			
-			def trackSession(session)
-				self.sessionHandler.trackSession(session)
-			end
-			
-			def parseTrackSession(raw)
-				self.sessionHandler.parseTrackSession(raw)
-			end
-			
-			protected :sessionHandler, :parseSession, :trackSession, :parseTrackSessio
 	
+			protected :sessionHandling
 		end
 		
 	end
@@ -134,23 +181,11 @@ module SwapDmi
 	
 	class Model
 		
-		def sessionHandler()
-			self.context.sessionHandler
+		def sessionHandling()
+			self.context.sessionHandling
 		end
 		
-		def parseSession(raw)
-			self.sessionHandler.parseSession(raw)
-		end
-		
-		def trackSession(session)
-			self.sessionHandler.trackSession(session)
-		end
-		
-		def parseTrackSession(raw)
-			self.sessionHandler.parseTrackSession(raw)
-		end
-		
-		protected :sessionTracker, :parseSession, :trackSession, :parseTrackSession
+		protected :sessionHandling
 		
 	end
 		
