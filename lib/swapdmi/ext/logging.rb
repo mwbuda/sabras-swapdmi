1137932d2cc9703a5fc77fdc35789949d4af753c
mwbuda <mbuda@sabrassoft.com>
2015-09-15 19:32:47 -0400

    rewrite logging extension to use basic Ruby STD-LIB loggers, instead of making our own.
    also simplify basic logging call, and design in general
    (no longer can get the overall logging index)

--- lib/swapdmi/ext/logging.rb
+++ lib/swapdmi/ext/logging.rb
@@ -1,37 +1,27 @@
 
+require 'logger'
+
 #
-# add logging capability
+# add logging capability, using the std-lib logger library
 #
 # This extension integrates with the base session handling extension, if that extension is present.
 #	SessionHandling instances will have logging
 #
 # logging is seemlessly supported at model, datasource, context, and impl levels
-#	thru a simple log method, wh/ takes a level and a message argument
+#
+# A simplified 'log' method is provided which uses a default severity of info.
+#	this default can be modified at both the instance, class, and masterclass level, and will obey inheritance
 # 
 # the default configuration simply prints to console.
 #
 #
 module SwapDmi
 	SwapDmi.declareExtension(:logging) 
-	
-	DefaultLogging = Proc.new {|level, m| puts "SwapDMI #{level}: #{m}"}
-	
-	class Logger
-		extend TrackClassHierarchy
 		
-		def initialize(id,&behavior)
-			assignId(id)
-			@func = behavior
-		end
-		
-		def log(level,m)
-			@func.call(level,m)
-		end
-		
-	end
-		
-	SwapDmi::Logger.new(:default, &DefaultLogging)
-	
+	LogRegistry = SwapDmi::Registry.new(Logger)
+	LogRegistry.defaultId = :default
+	LogRegistry.register(:default, Logger.new(STDOUT))
+
 	module HasLog
 		
 		def self.extended(base)
@@ -42,34 +32,60 @@ module SwapDmi
 			base.instance_eval { include SwapDmi::HasLog::Instance }
 		end
 		
-		def logging()
-			self.class_variable_get(:@@logging)
-		end
-		
 		def defineLogger(id, logid)
-			self.logging[id] = logid
+			self.class_variable_get(:@@logging)[id] = logid
 			self
 		end
 		
 		def logger(id)
-			logid = self.logging[id]
-			SwapDmi::Logger[logid]
+			logid = self.class_variable_get(:@@logging)[id]
+			SwapDmi::LogRegistry[logid]
+		end
+		
+		def defaultLoggingSeverity=(sev)
+			@swapdmi_dfLogSev = sev
+		end
+		def setDefaultLoggingSeverity(sev)
+			self.defaultLoggingSeverity = sev
+			self
+		end 
+		def defaultLoggingSeverity()
+			@swapdmi_dfLogSev
 		end
 		
 		module Instance
+			attr_accessor :defaultLoggingSeverity
+			
 			def defineLogger(logid)
-				self.class.logging[self.id] = logid
+				self.class.defineLogger(self.id,logid)
 				self
 			end
 			
 			def logger()
-				logid = self.class.logging[self.id]
-				SwapDmi::Logger[logid]
+				logid = self.class.logger(self.id)
 			end
 			
-			def log(level,m)
-				self.logger.log(level,m)
+			def setDefaultLoggingSeverity(sev)
+				self.defaultLoggingSeverity = sev
 				self
+			end
+			
+			# log with default value for severity
+			#	default severity can be set on object instance, or at the class, superclass, and masterclass level
+			#	default severity if nothing is configured is info.
+			#	progname & missing message block are considered to be nil. standard behavior with loggers applies
+			def log(m)
+				sev = Logger::Severity::INFO
+				recvs = [self, self.class, self.class.superclass, self.class.masterclass].uniq
+				recvs.each do |recv|
+					next unless recv.respond_to?(:defaultLoggingSeverity)
+					xv = recv.defaultLoggingSeverity
+					next if xv.nil?
+					sev = xv
+					break
+				end
+				
+				self.logger.log(sev, m, nil, &nil)
 			end
 		end
 		
@@ -88,8 +104,8 @@ module SwapDmi
 			self.context.logger
 		end
 		
-		def log(level,m)
-			self.logger.log(level,m)
+		def log(m)
+			self.context.log(m)
 		end
 	end
 
