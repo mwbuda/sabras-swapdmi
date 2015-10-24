03c769d470e08a8cf95b6d464a2214c41b7a1061
mwbuda <mbuda@sabrassoft.com>
2015-10-24 00:19:47 -0400

    in progress work on unit test for caching ext.

--- test/caching.rb
+++ test/caching.rb
@@ -4,29 +4,58 @@ require 'swapdmi'
 require 'swapdmi/ext/caching'
 
 #
+def assertTrue(expression)
+	throw :assert unless expression
+end
+
+def assertFalse(expression)
+	throw :assert if expression
+end
+
+#
 # check module inclusion
 #
 puts 'Checking if extension has loaded - Caching Extension'
 throw :assertExtension unless SwapDmi.hasExtensions?(:caching)
 
-
 #check cache readying
 
 #	can't use cache w/ out def save & get
-	#TODO
+	did_bork = false
+	BorkedCache = SwapDmi::Cache.new(:borked)
+	begin
+		BorkedCache.has?(1)
+	rescue SwapDmi::CacheSetupError => e
+		did_bork = true
+	end
+	assertTrue(did_bork)
 
 #	ready cache, ensure can't reconfigure
-	#TODO
+	TestCantReconfigCache = SwapDmi::Cache.new(:testCantReconfig)
+	SwapDmi::DefaultCacheLogic.configure(TestCantReconfigCache)
+	TestCantReconfigCache.ready
+	
+	did_bork = false
+	begin
+		TestCantReconfigCache.defineHas {|k| true}
+	rescue SwapDmi::CacheReconfigureError => e
+		did_bork = true
+	end
+	assertTrue(did_bork)
 
 #check the default cache logic
 
+	DefaultCache = SwapDmi::Cache.default
+	DefaultCache.defineEvictWhen(:all)
+	
 #	cache key matching
 	#TODO
-
+	
 #	data in, data out 
 	#TODO
 
 #	eviction (evict time = 0)
+	DefaultCache.config[:evictTime] = 0
 	#TODO
 
 # Test is completed - Output stuff
