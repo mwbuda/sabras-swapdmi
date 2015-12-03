5cdb7c0ad23536c0cf15b885c5dfe284cc3b98c1
mwbuda <mbuda@sabrassoft.com>
2015-12-02 22:45:50 -0500

    proxy: fix issue where could not resolve SwapDmi.idValue.
    This commit also contains a giant amount of junk rails.logger statements for debugging
    these are to be removed ASAP

--- lib/swapdmi/framework/proxy.rb
+++ lib/swapdmi/framework/proxy.rb
@@ -1,4 +1,7 @@
 
+#TODO: delete me
+require 'action_controller'
+
 module SwapDmi
 	
 	# a very basic proxy object to setup dynamic delegate behavior
@@ -6,10 +9,19 @@ module SwapDmi
 	class ProxyObject < BasicObject
 		
 		def initialize(registry, key = nil, &keyproc)
+			#TODO delete me
+			$railsLogger.debug("create SwapDmi Proxy => (#{registry}.#{keyproc ? '???' : key} <= #{keyproc})")
+			
 			@registry = registry
-			@key = key
-			@keyproc = keyproc
 			@filters = {}
+			
+			unless keyproc
+				@key = idValue(key)
+			else
+				@keyproc = keyproc
+				@mutable = key ? true : false
+				@setkey = false	
+			end
 		end
 		
 		def withProxyPreFilter(methodName, &filter)
@@ -18,24 +30,46 @@ module SwapDmi
 		end
 		
 		def proxyObjectKey()
-			if !@keyproc.nil?
-				self.instance_exec(&@keyproc)
-			elsif !@key.nil?
-				@key
+			return @key unless @keyproc
+			
+			unless @mutable && @setkey
+					@key = idValue( self.instance_exec(&@keyproc) )
+					@setkey = true
+					@key
 			else
-				nil
+					@key
 			end
 		end
 		
 		def proxyObjectDelegate()
-			@registry[self.proxyObjectKey]
+			key = self.proxyObjectKey
+			res = @registry[key]
+			
+			#TODO: delete me
+			$railsLogger.debug("proxy delegate: #{@registry} => #{key}::#{key.class} => #{res}")
+			res
+		end
+			
+		def idValue(rawid)
+			$railsLogger.debug("proxy kludge swap dmi id value (#{rawid})")
+			SwapDmi.idValue(rawid)
 		end
 			
 		def method_missing(method, *args, &proc)
+			#TODO delete me
+			$railsLogger.debug("proxy delegate: call #{method}")
+			$railsLogger.debug("proxy delegate: filters for #{method}? => #{!@filters[method].nil?}")
+			
 			filter = @filters[method]
 			self.instance_exec(method, *args, &filter) unless filter.nil?
+			
+			#TODO delete me
+			$railsLogger.debug("proxy delegate: ran filter for #{method}")
+			
 			self.proxyObjectDelegate.send(method, *args, &proc) 
 		end 
 	end
+	
+	ProxyObject::SwapDmi = self
 	
 end
