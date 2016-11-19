308edc91f698c16bc8a0d61fed1878bc0304b6d9
mwbuda <mbuda@sabrassoft.com>
2016-11-19 21:09:49 +0000

    update basic caching test

--- test/caching.rb
+++ test/caching.rb
@@ -38,21 +38,21 @@ puts 'test cache key matching'
 
 	#raw main key exact match
 	assertTrue( 
-		SwapDmi::DefaultCacheLogic::CacheKeyCompare.call(:raw, :raw)
+		SwapDmi::DefaultCacheKeyLogic::CacheKeyCompare.call(:raw, :raw)
 	)
 	assertFalse( 
-		SwapDmi::DefaultCacheLogic::CacheKeyCompare.call(:raw, :wrong)
+		SwapDmi::DefaultCacheKeyLogic::CacheKeyCompare.call(:raw, :wrong)
 	)
 	assertFalse( 
-		SwapDmi::DefaultCacheLogic::CacheKeyCompare.call(:wrong, :raw)
+		SwapDmi::DefaultCacheKeyLogic::CacheKeyCompare.call(:wrong, :raw)
 	)
 
 	#raw main key wildcard match
 	assertTrue( 
-		SwapDmi::DefaultCacheLogic::CacheKeyCompare.call(SwapDmi::CacheKey::Wildcard, :raw)
+		SwapDmi::DefaultCacheKeyLogic::CacheKeyCompare.call(SwapDmi::CacheKey::Wildcard, :raw)
 	)
 	assertTrue( 
-		SwapDmi::DefaultCacheLogic::CacheKeyCompare.call(:raw, SwapDmi::CacheKey::Wildcard)
+		SwapDmi::DefaultCacheKeyLogic::CacheKeyCompare.call(:raw, SwapDmi::CacheKey::Wildcard)
 	)
 
 	#check valid id
